51822419cfd610e27b25ef7d1b884b8a1bb1c1f3
saulogrund <sbicalho86@gmail.com>
2019-09-06 11:14:12 -0300

    comments

--- bp/models.py
+++ bp/models.py
@@ -3,4 +3,36 @@ from django.db import models
 class balanco_patrimonial(models.Model):
     empresa = models.CharField()
     ano = models.IntegerField()
-# Create your models here.
+
+    nome = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False
+    )
+
+    sobrenome = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False
+    )
+
+    cpf = models.CharField(
+        max_length=14,
+        null=False,
+        blank=False
+    )
+
+    tempo_de_servico = models.IntegerField(
+        default=0,
+        null=False,
+        blank=False
+    )
+
+    remuneracao = models.DecimalField(
+        max_digits=8,
+        decimal_places=2,
+        null=False,
+        blank=False
+    )
+
+    objetos = models.Manager()
